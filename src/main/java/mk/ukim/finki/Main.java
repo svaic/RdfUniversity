52a36af3bc32d9ced7d30f850d564fb76fd3808f
ljovanov <luka.jovanovski@netcetera.com>
2023-08-11 23:24:51 +0200

    change

--- src/main/java/mk/ukim/finki/Main.java
+++ src/main/java/mk/ukim/finki/Main.java
@@ -1,7 +1,99 @@
 package mk.ukim.finki;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.apache.jena.query.*;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var un = executeQuery();
+        mongo(un);
+    }
+
+    public static List<University> executeQuery() {
+
+        String sparqlQuery = "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n"
+                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                + "PREFIX dbo: <http://dbpedia.org/ontology/>\n"
+                + "PREFIX dbp: <http://dbpedia.org/property/>\n"
+                + "\n"
+                + "SELECT ?place ?name (sample(?longitude) as ?long) (sample(?latitude) as ?lat)\n"
+                + "       (sample(?image) as ?img)\n"
+                + "WHERE {\n"
+                + "  ?place geo:long ?longitude ;\n"
+                + "         geo:lat ?latitude ;\n"
+                + "         foaf:depiction ?image ;\n"
+                + "         dbp:name ?name ;\n"
+                + "         rdf:type dbo:University .\n"
+                + "\n"
+                + "  FILTER(isLiteral(?name))\n"
+                + "}\n"
+                + "GROUP BY ?place ?name";
+
+
+        List<University> universities = new ArrayList<>();
+
+        Query query = QueryFactory.create(sparqlQuery);
+
+        try (QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query)) {
+            ResultSet results = qexec.execSelect();
+
+            while (results.hasNext()) {
+                QuerySolution solution = results.nextSolution();
+                String name = solution.get("name").toString();
+                String longitude = solution.get("long").toString();
+                String latitude = solution.get("lat").toString();
+                String image = solution.get("img").toString();
+                String place = solution.get("place").toString();
+
+                University university = new University(name, longitude, latitude, image, place);
+                universities.add(university);
+
+                System.out.println("Place: " + place);
+                System.out.println("Longitude: " + longitude);
+                System.out.println("Latitude: " + latitude);
+                System.out.println("Image: " + image);
+                System.out.println("-----------------------");
+            }
+        }
+
+        return universities;
+    }
+
+    public static void mongo(List<University> universities) {
+        ConnectionString connectionString = new ConnectionString("");
+
+        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+
+        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                pojoCodecRegistry);
+
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .codecRegistry(codecRegistry)
+                .build();
+
+        try (MongoClient mongoClient = MongoClients.create(clientSettings)) {
+
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("banks");
+            mongoDatabase.createCollection("universities");
+
+            MongoCollection<University> mongoCollection = mongoDatabase.getCollection("universities", University.class);
+
+            mongoCollection.insertMany(universities);
+        }
     }
 }
--- src/main/java/mk/ukim/finki/University.java
+++ src/main/java/mk/ukim/finki/University.java
@@ -1,2 +1,57 @@
-package mk.ukim.finki;public class University {
+package mk.ukim.finki;
+
+public class University {
+    String name;
+    String image;
+    String longitute;
+    String latitude;
+    String place;
+
+    public University(String name, String image, String longitute, String latitude, String place) {
+        this.name = name;
+        this.image = image;
+        this.longitute = longitute;
+        this.latitude = latitude;
+        this.place = place;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLongitute() {
+        return longitute;
+    }
+
+    public void setLongitute(String longitute) {
+        this.longitute = longitute;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
 }
